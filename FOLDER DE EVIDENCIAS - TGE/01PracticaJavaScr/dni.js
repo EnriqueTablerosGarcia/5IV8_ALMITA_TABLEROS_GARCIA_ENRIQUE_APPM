bae0b073b207a59b85913d8835632e9037478f09
EnriqueTablerosGarcia <etablerosg2300@alumno.ipn.mx>
2025-09-21 20:45:03 -0600

    CAMBIOS EN EL CSS, JS E INDEX

--- FOLDER DE EVIDENCIAS - TGE/01PracticaJavaScr/dni.js
+++ FOLDER DE EVIDENCIAS - TGE/01PracticaJavaScr/dni.js
@@ -2,3 +2,14 @@
 function validarDNI() {
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 }
+var numero = document.getElementById('dni_numero').value;
+    var letraUsuario = document.getElementById('dni_letra').value.toUpperCase();
+    var mensaje = document.getElementById('mensaje');
+
+
+    if (numero < 0 || numero > 99999999) {
+        mensaje.textContent = 'El número proporcionado no es válido.';
+        mensaje.style.color = 'red';
+        return;
+    }
+   var letraCalculada = letras[numero % 23];
